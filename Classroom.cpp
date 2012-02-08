7a10f18aac59638c7d20fcba742d71888bc3fb22
bicodul <ludovic.robin.info@gmail.com>
2012-02-08 18:09:04 +0100

    coucou

---
+++ Classroom.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Classroom.cpp
+ * Author: bicodul
+ * 
+ * Created on February 8, 2012, 6:08 PM
+ */
+
+#include "Classroom.h"
+
+Classroom::Classroom() {
+}
+
+Classroom::Classroom(const Classroom& orig) {
+}
+
+Classroom::~Classroom() {
+}
+
