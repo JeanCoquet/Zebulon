2fb616331d4b312f0b87918110d5321df715802b
bicodul <ludovic.robin.info@gmail.com>
2012-02-08 18:09:15 +0100

    coucou

---
+++ Classroom.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Classroom.h
+ * Author: bicodul
+ *
+ * Created on February 8, 2012, 6:08 PM
+ */
+
+#ifndef CLASSROOM_H
+#define	CLASSROOM_H
+
+class Classroom {
+public:
+    Classroom();
+    Classroom(const Classroom& orig);
+    virtual ~Classroom();
+private:
+
+};
+
+#endif	/* CLASSROOM_H */
+
